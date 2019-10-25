9f4739ef27af8d969b9619babb4524ae9b0704f6
dwang-csi <dwang@csisoft.com>
2019-10-25 17:49:21 -0400

    Updated web api tester tool

--- WebApiTesterCode/WebApiTester/CreateBatch.xaml.cs
+++ WebApiTesterCode/WebApiTester/CreateBatch.xaml.cs
@@ -1,18 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
-<<<<<<< HEAD
 using System.Linq;
-=======
->>>>>>> f0fd3c20693e9ed9f732fd6242749658cfaccad9
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Text;
 using System.Windows;
-<<<<<<< HEAD
 using System.Windows.Controls;
-=======
->>>>>>> f0fd3c20693e9ed9f732fd6242749658cfaccad9
 using Microsoft.Win32;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
@@ -25,7 +19,6 @@ namespace WebApiTester
     public partial class CreateBatch
     {
         //private static string FilePath = "";
-<<<<<<< HEAD
         private static List<BatchDocumentInfo> batchDocsList;
         private static int sequence;
         private static HttpClient client;
@@ -111,26 +104,12 @@ namespace WebApiTester
             }
         }
 
-=======
-        private static List<BatchDocumentInfo> batchDocsList = new List<BatchDocumentInfo>();
-        private static int sequence = 1;
-        public CreateBatch()
-        {
-            InitializeComponent();
-            DocumentsTextbox.Text = "Attached Documents: ";
-        }
-
->>>>>>> f0fd3c20693e9ed9f732fd6242749658cfaccad9
         private void ButtonBrowseClick(object sender, RoutedEventArgs e)
         {
             var dlg = new OpenFileDialog
             {
-<<<<<<< HEAD
                 Filter = "Image Files (*.tif, *.pdf)|*.tif; *.pdf|All Files|*.*",
                 Multiselect = true
-=======
-                Filter = "Image Files (*.tif, *.pdf)|*.tif; *.pdf|All Files|*.*"
->>>>>>> f0fd3c20693e9ed9f732fd6242749658cfaccad9
             };
 
             // Show open file dialog box
@@ -138,7 +117,6 @@ namespace WebApiTester
             // Process open file dialog box results
             if (result == true)
             {
-<<<<<<< HEAD
                 
                 //if (string.IsNullOrWhiteSpace(docType)) docType = BatchClassComboBox.SelectionBoxItem.ToString();
                 // Open document 
@@ -165,26 +143,6 @@ namespace WebApiTester
                         DocumentsTextbox.Text += $"\n{sequence}. {docType} -> {fileName} -> dataStream";
                         sequence++;
                     }
-=======
-                // Open document 
-                if (FilePathCheckBox.IsChecked == true)
-                {
-                    var filePath = dlg.FileName;
-                    var fileName = Path.GetFileName(filePath);
-                    batchDocsList.Add(new BatchDocumentInfo(fileName, filePath, null, String.IsNullOrWhiteSpace(DocTypeTextbox.Text) ? null : DocTypeTextbox.Text));
-                    DocumentsTextbox.Text += $"\n{sequence}. {fileName}";
-                    sequence++;
-                }
-                else
-                {
-                    var filePath = dlg.FileName;
-                    var fileName = Path.GetFileName(filePath);
-                    byte[] bytes = File.ReadAllBytes(dlg.FileName);
-                    var data = Convert.ToBase64String(bytes);
-                    batchDocsList.Add(new BatchDocumentInfo(fileName, null, data, String.IsNullOrWhiteSpace(DocTypeTextbox.Text) ? null : DocTypeTextbox.Text));
-                    DocumentsTextbox.Text += $"\n{sequence}. {fileName}";
-                    sequence++;
->>>>>>> f0fd3c20693e9ed9f732fd6242749658cfaccad9
                 }
 
             }
@@ -192,7 +150,6 @@ namespace WebApiTester
 
         private void Button_CreateBatch(object sender, RoutedEventArgs e)
         {
-<<<<<<< HEAD
             try
                 {
                     if (BatchClassComboBox.SelectedItem == null || WorkflowComboBox.SelectedItem == null)
@@ -270,78 +227,6 @@ namespace WebApiTester
                 }
         }
         
-=======
-            HttpClient client = new HttpClient();
-            try
-            {
-                //var batchdoc = new BatchDocumentInfo((FilePathCheckBox.IsChecked != null && FilePathCheckBox.IsChecked == true) ? "" : Path.GetFileName(FilePath), (FilePathCheckBox.IsChecked != null && FilePathCheckBox.IsChecked == true) ? FilePath : "", (FilePathCheckBox.IsChecked != null && FilePathCheckBox.IsChecked == true) ? "" : FilePathTextbox.Text, DocTypeTextbox.Text);
-                int workflowId = 0;
-
-                if (!string.IsNullOrWhiteSpace(WorkflowTextbox.Text))
-                {
-                    bool canConvert = int.TryParse(WorkflowTextbox.Text, out workflowId);
-                    if (!canConvert)
-                    {
-                        StatusLabel.Content = "The input workflowID is not a valid integer.";
-                        return;
-                    }
-                }
-                //var batchDocs = new List<BatchDocumentInfo>{batchdoc}.ToArray();
-
-                var batchDocs = batchDocsList.ToArray();
-                string url = ((MainWindow)Application.Current.MainWindow).WebApiTextbox.Text;
-                BatchCreateInfo batchCreateInfo = new BatchCreateInfo(
-                    ClassNameTextbox.Text,
-                    BatchNameTextbox.Text,
-                    int.Parse(WorkflowTextbox.Text),
-                    RunIDTextbox.Text,
-                    batchDocs
-                );
-                StatusLabel.Content = "Creating Batch...";
-                client.BaseAddress = new Uri(url);
-                client.DefaultRequestHeaders.Accept.Clear();
-                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-           
-                var param = JsonConvert.SerializeObject(batchCreateInfo);
-                HttpContent contentPost = new StringContent(param, Encoding.UTF8, "application/json");
-
-                var response = client.PostAsync("/api/Batch/Create", contentPost).Result;
-                var result = response.Content.ReadAsStringAsync().Result;
-                if (response.IsSuccessStatusCode)
-                {
-                    var body = JObject.Parse(result);
-                    var BatchID = body["batchId"];
-
-                    ((MainWindow) Application.Current.MainWindow).BatchIDTextbox.Text = BatchID.ToString();
-                    ((MainWindow) Application.Current.MainWindow).NoticeTextbox.Text =
-                        $"Batch ID: {BatchID} successfully created for batch {batchCreateInfo.name}."
-                        + Environment.NewLine
-                        + JValue.Parse(result).ToString(Formatting.Indented);
-                    StatusLabel.Content = $"Batch ID: {BatchID} successfully created";
-                }
-                else
-                {
-                    StatusLabel.Content = $"Failed to create the batch {batchCreateInfo.name}. \nSee main windows for detailed error message.";
-                    ((MainWindow) Application.Current.MainWindow).BatchIDTextbox.Text = "";
-
-                    try
-                    {
-                        ((MainWindow) Application.Current.MainWindow).NoticeTextbox.Text =
-                            ($"Failed to create the batch {batchCreateInfo.name}. Exception: {result}");
-                    }
-                    catch (Exception)
-                    {
-                        ((MainWindow) Application.Current.MainWindow).NoticeTextbox.Text =
-                            ($"Failed to create the batch {batchCreateInfo.name}. No result from the Web Api.");
-                    }
-                }
-            }
-            catch (Exception ex)
-            {
-                StatusLabel.Content = $"Error: {ex.Message}. \nPlease double check url and parameters";
-            }
-        }
->>>>>>> f0fd3c20693e9ed9f732fd6242749658cfaccad9
 
         private void BatchNameTextbox_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
         {
@@ -359,7 +244,6 @@ namespace WebApiTester
             sequence = 1;
             batchDocsList = new List<BatchDocumentInfo>();
         }
-<<<<<<< HEAD
 
         private  void BatchClassComboBox_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
         {
@@ -435,7 +319,5 @@ namespace WebApiTester
         //    //}
             
         //}
-=======
->>>>>>> f0fd3c20693e9ed9f732fd6242749658cfaccad9
     }
 }
