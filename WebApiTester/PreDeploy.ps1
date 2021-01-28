e8b7b50bee39b6e3339d664d851b98ca360fa65e
mhernandez <mhernandez@csisoft.com>
2021-01-28 18:15:27 -0500

    Setup the Octo deployment package

---
+++ WebApiTester/PreDeploy.ps1
@@ -0,0 +1,17 @@
+﻿#Gets the installation folder
+$installDir = $OctopusParameters["OctopusOriginalPackageDirectoryPath"]
+$pscriptDir = Split-Path -Parent -Path $MyInvocation.MyCommand.Definition
+
+Write-Host "Install Directory $installDir"
+Write-Host "PS Script Directory $pscriptDir"
+
+#Gets CSI PS Library
+$csiOctoLib = [System.IO.Path]::Combine($pscriptDir, 'CsiOctoPackLib.ps1')
+$csiPsLib = [System.IO.Path]::Combine($pscriptDir, 'CsiPsLib.ps1')
+
+#Load CSI PS Library
+. $csiOctoLib
+. $csiPsLib
+
+Kill-Process -ProcessName "WebApiTester"
+
